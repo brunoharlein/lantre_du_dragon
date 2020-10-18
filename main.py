b735e500e9ab9947573a4647aa78ead660b25947
brunoharlein <bruno@harlein.com>
2020-10-18 18:10:02 +0200

    bug and debug :-)

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 import random
 import time
 
-    if __name__ == '__main__':
-        display_intro()
 
-    def display_intro():
-        print("Tu es dans un pays rempli de dragons. Devant toi, tu vois deux grottes. Dans l'une, le dragon Thomas est amical et partagera son trésor avec toi. Dans l'autre, le dragon Yamina est affamé et te dévorera s'il te voit")
-        # print()
+def displayIntro():
+    print('''Tu es dans un pays rempli de dragons. Devant toi, tu vois deux grottes
+Dans l'une d'elle, le dragon Thomas est amical et partagera son trésor avec toi.
+Dans l'autre grotte, le dragon Yamina est affamé et te dévorera s'il te voit''')
+    print()
 
-    def choose_cave():
-        cave = ""
-        while cave != "1" and cave != "2":
-            print("Dans quelle grotte vas-tu entrer (1 ou 2) ? ")
-            cave = input()
-        return cave
 
-    def check_cave(chosen_cave):
-        print("Tu t'approches de la grotte ...")
-        time.sleep(2)
-        print("Tout est sombre et effrayant ...")
-        time.sleep(2)
-        print("Un énorme dragon saute juste devant toi ! "
-        + " Il ouvre grand ses machoires et ...")
-        print()
-        time.sleep(2)
+def chooseCave():
+    cave = ""
+    while cave != "1" and cave != "2":
+        print('Dans quelle grotte vas-tu entrer ? (1 or 2)')
+        cave = input()
 
-        friendly_cave = random.randint(1,2)
+    return cave
 
-        if chosen_cave == str(friendly_cave):
-            print("Te donne son trésor !")
-        else:
-            print("T'avale d'un seul coup !")
-        print()
 
-        play_again = "oui"
+def checkCave(chosenCave):
+    print("Tu t'approches de la grotte")
+    time.sleep(2)
+    print("Tout est sombre et effrayant")
+    time.sleep(2)
+    print("Un énorme dragon saute juste devant toi ! Il ouvre grand ses machoires et ...")
+    print()
+    time.sleep(2)
 
-        while play_again == "oui" or play_again == "o":
-            display_intro()
-            cave_number = choose_cave()
-            check_cave(cave_number)
+    friendlyCave = random.randint(1, 2)
 
-            print("Veux-tu rejouer (oui ou non) ?")
-            play_again = input()
+    if chosenCave == str(friendlyCave):
+        print("Te donne son trésor !")
+    else:
+        print("T'avale d'un seul coup !")
+
+
+playAgain = "oui"
+while playAgain == "oui" or playAgain == "y":
+    displayIntro()
+    caveNumber = chooseCave()
+    checkCave(caveNumber)
+
+    print("Veux-tu rejouer (oui ou non) ?")
+    playAgain = input()
